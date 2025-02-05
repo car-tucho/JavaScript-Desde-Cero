1874c33df47d2a39194a5e35db0f76aeaeead42b
Carlos <car_tucho@outlook.com>
2025-02-04 20:24:03 -0600

    Ejercicio de práctica resuelto

---
+++ tipos-de-dato.js
@@ -0,0 +1,55 @@
+
+//Este es un comentario en Javascript
+//Console.log es una función que se utiliza para imprimir en cosnsola
+
+console.log('Hola mundo');
+
+//Declaración de variables
+var nombre = "Ana";//String
+var edad = 23; // Number
+var sueldo = 1.2365; //Number
+var casada = true;//Boolean
+var vacunas = false;//Boolean
+var hijos = null; //Null
+var dato; //Undefined
+
+//typeof se utuliza para saber el tipo de dato de una variable
+
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(sueldo));
+console.log(typeof(casada));
+console.log(typeof(vacunas));
+console.log(typeof(hijos));
+console.log(typeof(dato));
+
+//La semantica es importante, significsdo de las palabras
+var nombre = "Francia"
+var país = 20;
+
+//NaN Not a number es un valor especial indica matematicamente no es un número.
+
+console.log(10*"Carlos");
+
+/*Si ponemos typeof de null dirá que es un objeto, pero en realidad es ausencia intencional de valor. 
+
+Esto es debido a un error en JavScript que aún no se ha corregido por compatibilidad de versiones anteriores.*/
+
+//Comentario en línea
+/*Comentario 
+en bloque*/
+
+//Ejercicios
+
+console.log(typeof(42));
+console.log(typeof('Veinticinco'));
+console.log(typeof(-666));
+console.log(typeof(true));
+console.log(typeof(0));
+console.log(typeof(''));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(false));
+console.log(typeof(58929));
+console.log(typeof(2.5644));
+console.log(typeof('DEVF'));
